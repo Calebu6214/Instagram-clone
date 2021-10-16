83d4a22c71f881d34a58811a5f681c7fb5cfeafb
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-16 12:12:40 +0300

    add profile and image form

---
+++ insta/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from  .models import Profile,Comments,Image
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from urllib import request
+        
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','image_name','image_caption']
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_image','name','bio','email']
+        

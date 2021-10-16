c301b818c8d18105cc50f3a072d8add625a56563
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-16 12:28:02 +0300

    add urls for craeted views

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django .utils import timezone
+import datetime as dt
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
---
+++ insta/urls.py
@@ -0,0 +1,15 @@
+ from django.urls import path
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    
+    path('registration/', views.registerPage, name="registration"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

d88a03db86ef2f5c6a4c14ccdb327830084ed349
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-16 16:11:19 +0300

    add login image

--- insta/urls.py
+++ insta/urls.py
@@ -1,4 +1,4 @@
- from django.urls import path
+from django.urls import path
 from django.conf.urls import url
 from . import views
 from django.conf import settings
@@ -6,7 +6,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-    
+    path('',views.indexPage, name='home'),
+    path('profile/', views.profile,name='profile'),
+    url('edit/', views.edit_profile,name='edit_profile'),
+    url('user/',views.search_username,name='search_username'),
+    url('image/', views.upload_image,name='upload_image'),
+    url('likes/(\d+)/' , views.image_likes, name='likes'),
+    url('new_comment/(\d+)/' ,views.add_comment,name='Comments'),
+    path('post/', views.create_post,name='create_post'),
     path('registration/', views.registerPage, name="registration"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
--- instagram/settings.py
+++ instagram/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },

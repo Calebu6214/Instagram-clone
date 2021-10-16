4b7d7181fb1e94bd65ec8254e3eca5288695712c
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-16 11:40:53 +0300

    add registerpage  method view

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from . models import *
+from django.contrib.auth.forms import UserCreationForm
+# from .forms import ProfileForm,CommentsForm, ImageForm
+# from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = UserCreationForm()
+        if request.method == 'POST':
+            form =UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                # messages.success(request,'Account was created successfully')
+                return redirect('login')
+        context = {'form': form}
+        return render(request,'registration/registration_form.html',  context)
